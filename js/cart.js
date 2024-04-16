f533e2f161587a594db0f6e8f79655b062afb032
Mariela Morsucci <marielamorsu333@gmail.com>
2024-04-16 16:13:45 -0400

    js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,32 @@
+import {renderCardsCart} from "./modulo.js"
+
 console.log(data)
+//borrar LS
+//localStorage.clear('idsCarrito')
+
+
+let productosId=[]
+let LS=JSON.parse(localStorage.getItem('idsCarrito'))
+
+console.log(LS);
+
+if(LS){
+productosId = LS
+}
+
+console.log(productosId);
+let contenedorCards= document.getElementById('conteinerCart')
+
+
+let arrayProdComprados=[]
+
+for (const iterator of productosId) {
+let aux = data.find( obj => obj.id == parseInt(iterator))
+if(aux){
+    arrayProdComprados.push(aux)
+}     
+}
+
+
+console.log(arrayProdComprados);
+renderCardsCart(arrayProdComprados,contenedorCards)
